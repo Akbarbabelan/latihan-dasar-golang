7014b94d1d160464d53a518a0b9c83479d701f51
Akbarbabelan <babelan96@gmail.com>
2022-11-12 13:55:43 +0700

    latihan data string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Kholifah")
+	fmt.Println("Kholifah Akbar")
+	fmt.Println("Kholifah Akbar Babelan")
+
+	fmt.Println(len("Kholifah"))
+	fmt.Println("Kholifah Akbar"[0])
+	fmt.Println("Kholifah Akbar Babelan"[1])
+}
