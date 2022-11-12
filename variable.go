a0695401d848051131dab4dd1a3e5d76ffcd1cb0
Akbarbabelan <babelan96@gmail.com>
2022-11-12 14:04:26 +0700

    latihan variabel

---
+++ variable.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Kholifah Akbar"
+	fmt.Println(name)
+
+	name = "Akbar Babelan"
+	fmt.Println(name)
+
+	var friendName = "Musthofa"
+	fmt.Println(friendName)
+
+	var age = 38
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "Kholifah"
+		lastName  = "Musthofa"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
